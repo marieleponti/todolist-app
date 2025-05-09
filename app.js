a5cd9ddb9d1d02c9ad09b6310e030c5407d8b6de
Mariele Ponticiello <mariele.ponticiello@temple.edu>
2025-05-09 07:37:15 -0400

    remove use static

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const _ = require("lodash");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 
 /** server settings */
